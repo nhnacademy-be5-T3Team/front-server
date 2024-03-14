5d6259cda2115d11464c03106b495beed4c74d9c
PyeonMinjun <alswns9810@gmail.com>
2024-03-14 22:31:27 +0900

    ci : connection

---
+++ src/main/java/com/t3t/frontserver/index/indexController.java
@@ -0,0 +1,13 @@
+package com.t3t.frontserver.index;
+
+import lombok.Getter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class indexController {
+    @GetMapping("/")
+    public String test() {
+        return "index/index";
+    }
+}
