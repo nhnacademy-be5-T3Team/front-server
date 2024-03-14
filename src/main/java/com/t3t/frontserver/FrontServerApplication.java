611faf979295814f1d81c4aceba66b45c00c88f0
PyeonMinjun <alswns9810@gmail.com>
2024-03-14 22:17:28 +0900

    init

---
+++ src/main/java/com/t3t/frontserver/FrontServerApplication.java
@@ -0,0 +1,13 @@
+package com.t3t.frontserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrontServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FrontServerApplication.class, args);
+	}
+
+}
