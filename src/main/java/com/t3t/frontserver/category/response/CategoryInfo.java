dc08dfcfc495979f27275c5fbe47da6c234d3b58
yujinKim <yujinkimvv@gmail.com>
2024-04-08 02:14:16 +0900

    feat : #11 카테고리 전체 목록 조회 api 관련 feign client 요청 구현

--- src/main/java/com/t3t/frontserver/category/response/CategoryInfo.java
+++ src/main/java/com/t3t/frontserver/category/response/CategoryInfo.java
@@ -1,2 +1,9 @@
-package com.t3t.frontserver.category.response;public class CategoryInfo {
+package com.t3t.frontserver.category.response;
+
+import lombok.Getter;
+
+@Getter
+public class CategoryInfo {
+    private Integer id;
+    private String name;
 }
