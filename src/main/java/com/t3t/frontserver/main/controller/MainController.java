d30af973248cd9a0e0001c920b833bfd04cc8782
yujinKim <yujinkimvv@gmail.com>
2024-04-07 14:57:04 +0900

    feat : #11 메인 홈페이지 ItemBox fragment 분리

--- src/main/java/com/t3t/frontserver/main/controller/MainController.java
+++ src/main/java/com/t3t/frontserver/main/controller/MainController.java
@@ -1,13 +1,13 @@
-package com.t3t.frontserver.index;
+package com.t3t.frontserver.main.controller;
 
-import lombok.Getter;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class indexController {
+public class MainController {
+
     @GetMapping("/")
-    public String test() {
+    public String homeView() {
         return "main/page/home.html";
     }
 }
