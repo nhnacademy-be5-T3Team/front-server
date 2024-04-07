a5ed7fd34bb827fb38dcf074ad70cad1f6333a81
yujinKim <yujinkimvv@gmail.com>
2024-04-07 22:28:02 +0900

    feat : #11 resposne dto 생성

--- src/main/java/com/t3t/frontserver/main/response/BookInfoBrief.java
+++ src/main/java/com/t3t/frontserver/main/response/BookInfoBrief.java
@@ -1,10 +1,8 @@
-package com.t3t.frontserver.recommendation.response;
+package com.t3t.frontserver.main.response;
 
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class BookInfoBrief {
     private String name; // 도서 제목
     private String coverImageUrl; // 도서 커버 이미지 url
--- src/main/java/com/t3t/frontserver/model/response/BaseResponse.java
+++ src/main/java/com/t3t/frontserver/model/response/BaseResponse.java
@@ -1,11 +1,9 @@
 package com.t3t.frontserver.model.response;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
 @NoArgsConstructor
 public class BaseResponse<T> {
@@ -17,34 +15,4 @@ public class BaseResponse<T> {
         this.message = message;
         this.data = data;
     }
-
-    public static <T> BaseResponse<T> success(String message, T data) {
-        return BaseResponse.<T>builder()
-                .message(message)
-                .data(data)
-                .build();
-    }
-
-    /**
-     * 응답 객체에 데이터를 설정한다.
-     * @param data 응답할 데이터
-     * @return BaseResponse
-     * @author woody35545(구건모)
-     */
-    public BaseResponse<T> data(T data) {
-        this.data = data;
-        return this;
-    }
-
-    /**
-     * 응답 객체에 메시지를 설정한다.
-     * @param message 응답할 메시지
-     * @return BaseResponse
-     * @author woody35545(구건모)
-     */
-    public BaseResponse<T> message(String message) {
-        this.message = message;
-        return this;
-    }
-
 }
