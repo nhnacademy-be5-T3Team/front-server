43948d5c0c865ea42bdcd7220edf73dd538b1f69
yujinKim <yujinkimvv@gmail.com>
2024-04-10 00:06:53 +0900

    feat : #12 도서 상세 조회 api 관련 response 구현

--- src/main/java/com/t3t/frontserver/book/model/response/AuthorInfo.java
+++ src/main/java/com/t3t/frontserver/book/model/response/AuthorInfo.java
@@ -1,10 +1,8 @@
-package com.t3t.bookstoreapi.book.model.response;
+package com.t3t.frontserver.book.model.response;
 
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class AuthorInfo {
     private String name;
     private String role;
--- src/main/java/com/t3t/frontserver/book/model/response/TagInfo.java
+++ src/main/java/com/t3t/frontserver/book/model/response/TagInfo.java
@@ -1,10 +1,8 @@
-package com.t3t.bookstoreapi.book.model.response;
+package com.t3t.frontserver.book.model.response;
 
-import lombok.Builder;
 import lombok.Getter;
 
 @Getter
-@Builder
 public class TagInfo {
     private int id;
     private String name;
