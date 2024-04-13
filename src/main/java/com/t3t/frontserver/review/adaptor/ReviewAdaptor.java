ecc172b5059cd18e12a368ffc8c1846da609d589
yujinKim <yujinkimvv@gmail.com>
2024-04-14 00:14:31 +0900

    feat : #12 리뷰 목록 요청 feign client parameter 수정

--- src/main/java/com/t3t/frontserver/review/adaptor/ReviewAdaptor.java
+++ src/main/java/com/t3t/frontserver/review/adaptor/ReviewAdaptor.java
@@ -1,17 +1,18 @@
 package com.t3t.frontserver.review.adaptor;
 
 import com.t3t.frontserver.model.response.BaseResponse;
+import com.t3t.frontserver.model.response.PageResponse;
 import com.t3t.frontserver.review.response.ReviewResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 //TODO : url 변경
 @FeignClient(name = "reviewAdaptor", url = "http://localhost:8081")
 public interface ReviewAdaptor {
     @GetMapping(value = "/reviews/book/{bookId}")
-    ResponseEntity<BaseResponse<Page<ReviewResponse>>> getBook(@PathVariable Long bookId, Pageable pageable);
+    ResponseEntity<BaseResponse<PageResponse<ReviewResponse>>> getBook(@PathVariable Long bookId, @RequestParam int pageNo);
 }
