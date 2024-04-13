1c3150abb92a7a72ecf66bfd328d28e7d222b773
yujinKim <yujinkimvv@gmail.com>
2024-04-14 00:15:22 +0900

    feat : #12 도서 상세페이지 관련 Controller 수정

--- src/main/java/com/t3t/frontserver/book/controller/BookController.java
+++ src/main/java/com/t3t/frontserver/book/controller/BookController.java
@@ -4,18 +4,18 @@ import com.t3t.frontserver.book.adaptor.BookAdaptor;
 import com.t3t.frontserver.book.model.response.BookDetailResponse;
 import com.t3t.frontserver.category.adaptor.CategoryAdaptor;
 import com.t3t.frontserver.category.response.CategoryListResponse;
+import com.t3t.frontserver.index.OrderFormRequest;
 import com.t3t.frontserver.model.response.BaseResponse;
+import com.t3t.frontserver.model.response.PageResponse;
 import com.t3t.frontserver.review.adaptor.ReviewAdaptor;
 import com.t3t.frontserver.review.response.ReviewResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
@@ -29,15 +29,31 @@ public class BookController {
     private final ReviewAdaptor reviewAdaptor;
 
     @GetMapping("books/{bookId}")
-    public String test(Model model, @PathVariable Long bookId, @PageableDefault(page = 0, size = 10) Pageable pageable) {
+    public String test(Model model, @PathVariable Long bookId,
+                       @RequestParam(value = "pageNo", defaultValue = "0", required = false) int pageNo) {
 
         List<CategoryListResponse> categoryList = getDataFromCategoryAdaptor();
         BookDetailResponse bookDetailList = getDataFromBookAdaptor(bookId);
-        //Page<ReviewResponse> reviewPage = getDataFromReviewAdaptor(bookId, pageable);
+        PageResponse<ReviewResponse> reviewList = getDataFromReviewAdaptor(bookId, pageNo);
+
+        // 리뷰 목록을 위한 페이징
+        if (reviewList != null) {
+            int blockLimit = 3; // 현재 페이지 앞뒤로 보여줄 개수 설정
+            int nowPage = reviewList.getPageNo() + 1;
+            int startPage = Math.max(nowPage - blockLimit, 1);
+            int endPage = Math.min(nowPage + blockLimit, reviewList.getTotalPages());
+
+            model.addAttribute("reviewNowPage", nowPage);
+            model.addAttribute("reviewStartPage", startPage);
+            model.addAttribute("reviewEndPage", endPage);
+            model.addAttribute("reviewList", reviewList.getContent());
+        }
 
         model.addAttribute("categoryList", categoryList);
         model.addAttribute("bookDetailList", bookDetailList);
-        //model.addAttribute("reviewList", reviewList);
+
+        // 주문 페이지로 넘기기 위한 객체 (도서 id, 주문 수량, 포장 id)
+        model.addAttribute("orderFormRequest", new OrderFormRequest(bookId));
 
         return "main/page/detail.html";
     }
@@ -52,8 +68,8 @@ public class BookController {
         return handleResponse(bookDetailResponse);
     }
 
-    private Page<ReviewResponse> getDataFromReviewAdaptor(Long bookId, Pageable pageable) {
-        ResponseEntity<BaseResponse<Page<ReviewResponse>>> reviewsResponse = reviewAdaptor.getBook(bookId, pageable);
+    private PageResponse<ReviewResponse> getDataFromReviewAdaptor(Long bookId, int pageNo) {
+        ResponseEntity<BaseResponse<PageResponse<ReviewResponse>>> reviewsResponse = reviewAdaptor.getBook(bookId, pageNo);
         return handleResponse(reviewsResponse);
     }
 }
