ab66de2c0a678f88a28a473463974b09ef835bea
yujinKim <yujinkimvv@gmail.com>
2024-04-14 14:01:05 +0900

    feat : #13 카테고리 별 도서 검색 요청 fegin client 구현

---
+++ src/main/java/com/t3t/frontserver/book/adaptor/BookCategoryAdaptor.java
@@ -0,0 +1,17 @@
+package com.t3t.frontserver.book.adaptor;
+
+import com.t3t.frontserver.book.model.response.BookSearchResultResponse;
+import com.t3t.frontserver.model.response.BaseResponse;
+import com.t3t.frontserver.model.response.PageResponse;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+
+//TODO : url 변경
+@FeignClient(name = "bookCategoryAdaptor", url = "http://localhost:8081")
+public interface BookCategoryAdaptor {
+    @GetMapping(value = "/category/{categoryId}/books")
+    ResponseEntity<BaseResponse<PageResponse<BookSearchResultResponse>>> getBooksByCategoryId(@PathVariable Long categoryId, @RequestParam int pageNo);
+}
