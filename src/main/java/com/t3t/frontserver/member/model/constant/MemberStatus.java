16bb55f2ac3dddb9c68b2ad348c969cb44b2d742
woody35545 <woody35545@gmail.com>
2024-04-15 09:29:42 +0900

    feat: #19 `MemberStatus` - 회원의 상태를 나타내는 enum

---
+++ src/main/java/com/t3t/frontserver/member/model/constant/MemberStatus.java
@@ -0,0 +1,10 @@
+package com.t3t.frontserver.member.model.constant;
+
+/**
+ * 회원의 상태를 나타내는 enum <br>
+ * 회원은 `ACTIVE(활성)`, `INACTIVE(비활성)`, `WITHDRAWAL(탈퇴)` 세 가지 상태를 가질 수 있다.
+ * @author woody35545(구건모)
+ */
+public enum MemberStatus {
+    ACTIVE, INACTIVE, WITHDRAWAL
+}
