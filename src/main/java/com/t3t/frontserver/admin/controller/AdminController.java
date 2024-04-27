200f97f2a82b3929734a98d81a7639ab9445ce42
yujinKim <yujinkimvv@gmail.com>
2024-04-27 21:36:08 +0900

    feat : #39 도서 관리자 view fragment 분리

---
+++ src/main/java/com/t3t/frontserver/admin/controller/AdminController.java
@@ -0,0 +1,13 @@
+package com.t3t.frontserver.admin.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AdminController {
+
+    @GetMapping("/testAdmin")
+    public String test() {
+        return "admin/page/test";
+    }
+}
