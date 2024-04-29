d0675744ab9e2cfb579448ae6e363c7a41094a35
yujinKim <yujinkimvv@gmail.com>
2024-04-30 00:11:02 +0900

    feat : #39 도서 등록 관리자 view - 목록 조회 api 연결

--- src/main/resources/static/assets/admin/js/adminBook.js
+++ src/main/resources/static/assets/admin/js/adminBook.js
@@ -116,3 +116,202 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 });
+
+/*
+ * Table Update 함수
+ */
+
+// 응답받은 데이터에 맞게 출판사 테이블의 내용을 업데이트하는 함수
+function updatePublisherTable(data) {
+    var tableBody = $('#publisherTable tbody');
+    tableBody.empty();
+    data.forEach(function(publisher) {
+        var row = $('<tr>');
+        // 각 행에 출판사 ID와 이름을 데이터로 저장
+        row.data('id', publisher.publisherId);
+        row.data('name', publisher.publisherName);
+
+        var checkboxCell = $('<td>');
+        var checkbox = $('<input>').attr('type', 'checkbox').attr('name', 'publisherCheckbox');
+        // checkbox에 클릭 이벤트 핸들러 등록 (하나만 선택될 수 있도록)
+        checkbox.on('click', function() {
+            $('input[name="publisherCheckbox"]').not(this).prop('checked', false);
+        });
+        checkboxCell.append(checkbox);
+        row.append(checkboxCell);
+        row.append($('<td>').text(publisher.publisherId));
+        row.append($('<td>').text(publisher.publisherName));
+        row.append($('<td>').text(publisher.publisherEmail));
+        tableBody.append(row);
+    });
+}
+
+// 응답받은 데이터에 맞게 참여자 선택 테이블의 내용을 업데이트하는 함수
+function updateParticipantTable(data) {
+    var tableBody = $('#participantTable tbody');
+    tableBody.empty();
+    data.forEach(function(participant) {
+        var row = $('<tr>');
+        // 각 행에 참여자 ID와 이름을 데이터로 저장
+        row.data('id', participant.id);
+        row.data('name', participant.name);
+
+        var checkboxCell = $('<td>');
+        var checkbox = $('<input>').attr('type', 'checkbox').attr('name', 'participantCheckbox');
+        // checkbox에 클릭 이벤트 핸들러 등록 (하나만 선택될 수 있도록)
+        checkbox.on('click', function() {
+            $('input[name="participantCheckbox"]').not(this).prop('checked', false);
+        });
+        checkboxCell.append(checkbox);
+        row.append(checkboxCell);
+        row.append($('<td>').text(participant.id));
+        row.append($('<td>').text(participant.name));
+        row.append($('<td>').text(participant.email));
+        tableBody.append(row);
+    });
+}
+
+// 응답받은 데이터에 맞게 참여자 역할 선택 테이블의 내용을 업데이트하는 함수
+function updateParticipantRoleTable(data) {
+    var tableBody = $('#participantRoleTable tbody');
+    tableBody.empty();
+    data.forEach(function(participantRole) {
+        var row = $('<tr>');
+        // 각 행에 참여자 역할 ID와 한국어 이름을 데이터로 저장
+        row.data('id', participantRole.id);
+        row.data('name', participantRole.roleNameKr);
+
+        var checkboxCell = $('<td>');
+        var checkbox = $('<input>').attr('type', 'checkbox').attr('name', 'participantRoleCheckbox');
+        // checkbox에 클릭 이벤트 핸들러 등록 (하나만 선택될 수 있도록)
+        checkbox.on('click', function() {
+            $('input[name="participantRoleCheckbox"]').not(this).prop('checked', false);
+        });
+        checkboxCell.append(checkbox);
+        row.append(checkboxCell);
+        row.append($('<td>').text(participantRole.id));
+        row.append($('<td>').text(participantRole.roleNameKr));
+        row.append($('<td>').text(participantRole.roleNameEn));
+        tableBody.append(row);
+    });
+}
+
+// 응답받은 데이터에 맞게 카테고리 선택 테이블의 내용을 업데이트하는 함수
+function updateCategoryTable(startDepth, maxDepth, data) {
+    var tableHead = $('#categoryTable thead');
+    var tableBody = $('#categoryTable tbody');
+
+    // 첫 번째 열 :  checkbox
+    var headerRow = $('<tr>');
+    headerRow.append('<th>Checkbox</th>');
+
+    // startDepth부터 maxDepth까지의 열을 추가
+    for (var depth = startDepth; depth <= maxDepth; depth++) {
+        headerRow.append('<th>' + depth + ' Depth</th>');
+    }
+
+    // thead에 행을 추가
+    tableHead.empty().append(headerRow);
+
+    tableBody.empty();
+
+    // 데이터를 처리하는 재귀 함수
+    function processCategories(categories, parentNames, parentId) {
+        categories.forEach(function(category) {
+            var row = $('<tr>');
+
+            // 카테고리 id를 설정합니다.
+            var categoryId = category.categoryId || parentId; // 자식 카테고리가 없는 경우 parentId를 사용
+            row.attr('data-category-id', categoryId);
+            row.attr('data-category-name', category.categoryName); // 카테고리 이름을 데이터로 추가
+
+            // 각 행에 첫 번째 열에는 checkbox를 추가
+            var checkboxCell = $('<td>');
+            var checkbox = $('<input>').attr('type', 'checkbox').attr('name', 'categoryCheckbox');
+            checkbox.on('click', function() {
+                var index = $(this).closest('tr').attr('data-category-id');
+                var value = $(this).closest('tr').attr('data-category-name');
+
+                var selectedCategoriesCount = $('#selectedCategoryInModal h6').length;
+                var isDuplicate = $('#selectedCategoryInModal').find('#' + index).length > 0;
+
+                if($(this).is(':checked')) {
+                    if (selectedCategoriesCount >= 10) {
+                        alert('10개 이상의 태그를 선택할 수 없습니다.');
+                        $(this).prop('checked', false);
+                    } else if (isDuplicate) {
+                        alert('이미 선택한 태그입니다.');
+                        $(this).prop('checked', false);
+                    } else {
+                        $('#selectedCategoryInModal').append('<h6 id="' + index + '" value="'+ value  +'">' + index + ' | ' + value + '</h6>');
+                    }
+                } else {
+                    $('#selectedCategoryInModal').find('#' + index).remove();
+                }
+            });
+            checkboxCell.append(checkbox);
+            row.append(checkboxCell);
+
+            // 각 행에 카테고리 이름을 추가
+            parentNames.forEach(function(parentName) {
+                var parentCategoryCell = $('<td>').text(parentName);
+                row.append(parentCategoryCell);
+            });
+            var categoryNameCell = $('<td>').text(category.categoryName);
+            row.append(categoryNameCell);
+
+
+
+            tableBody.append(row);
+
+            // 해당 카테고리에 하위 항목이 있는 경우 재귀적으로 처리
+            if (category.children && category.children.length > 0) {
+                var newParentNames = parentNames.slice();
+                newParentNames.push(category.categoryName);
+                processCategories(category.children, newParentNames, categoryId);
+            }
+        });
+    }
+    processCategories(data, [], null);
+}
+
+
+// 응답받은 데이터에 맞게 태그 선택 테이블의 내용을 업데이트하는 함수
+function updateTagTable(data) {
+    var tableBody = $('#tagTable tbody');
+    tableBody.empty();
+    data.forEach(function(tag) {
+        var row = $('<tr>');
+        // 각 행에 참여자 ID와 이름을 데이터로 저장
+        row.data('id', tag.id);
+        row.data('name', tag.name);
+
+        var checkboxCell = $('<td>');
+        var checkbox = $('<input>').attr('type', 'checkbox').attr('name', 'tagCheckbox').attr('value', tag.name).attr('index', tag.id);
+        checkbox.on('click', function() {
+            var index = $(this).attr('index');
+            var value = $(this).attr('value');
+            var selectedTagsCount = $('#selectedTagInModal h6').length;
+            var isDuplicate = $('#selectedTagInModal').find('#' + index).length > 0;
+
+            if($(this).is(':checked')) {
+                if (selectedTagsCount >= 10) {
+                    alert('10개 이상의 태그를 선택할 수 없습니다.');
+                    $(this).prop('checked', false);
+                } else if (isDuplicate) {
+                    alert('이미 선택한 태그입니다.');
+                    $(this).prop('checked', false);
+                } else {
+                    $('#selectedTagInModal').append('<h6 id="' + index + '" value="'+ value  +'">' + index + ' | ' + value + '</h6>');
+                }
+            } else {
+                $('#selectedTagInModal').find('#' + index).remove();
+            }
+        });
+        checkboxCell.append(checkbox);
+        row.append(checkboxCell);
+        row.append($('<td>').text(tag.id));
+        row.append($('<td>').text(tag.name));
+        tableBody.append(row);
+    });
+}
