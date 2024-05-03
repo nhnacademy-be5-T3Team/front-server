46464b28dcbc6e1f33359cc6c846ed4f5415c5e8
yujinKim <yujinkimvv@gmail.com>
2024-05-03 13:46:37 +0900

    feat : #39 도서 등록 API Client 생성

--- src/main/java/com/t3t/frontserver/book/client/BookApiClient.java
+++ src/main/java/com/t3t/frontserver/book/client/BookApiClient.java
@@ -1,14 +1,20 @@
 package com.t3t.frontserver.book.client;
 
+import com.t3t.frontserver.book.model.request.BookRegisterRequest;
 import com.t3t.frontserver.book.model.response.BookDetailResponse;
 import com.t3t.frontserver.model.response.BaseResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @FeignClient(name = "bookAdaptor", url = "${t3t.feignClient.url}")
 public interface BookApiClient {
     @GetMapping(value = "/t3t/bookstore/books/{bookId}")
     ResponseEntity<BaseResponse<BookDetailResponse>> getBook(@PathVariable Long bookId);
+
+    @PostMapping(value = "/t3t/bookstore/books")
+    ResponseEntity<BaseResponse<Void>> createBook(@RequestBody BookRegisterRequest request);
 }
