55bb538386d72f9c039b8612b0a5a0a6106cf1e9
woody35545 <woody35545@gmail.com>
2024-05-05 21:33:25 +0900

    modify: `GlobalExceptionHandler` - 예외 발생시 메시지 뷰로 리다이렉트 되도록 수정

--- src/main/java/com/t3t/frontserver/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/t3t/frontserver/common/exception/GlobalExceptionHandler.java
@@ -1,14 +1,20 @@
 package com.t3t.frontserver.common.exception;
 
+import com.t3t.frontserver.member.client.MemberApiClient;
+import com.t3t.frontserver.member.exception.MemberApiClientException;
+import feign.FeignException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
     @ExceptionHandler(Exception.class)
-    public String handleException(Model model, Exception e){
-        model.addAttribute("message", e.getMessage());
-        return "main/page/message";
+    public String handleException(RedirectAttributes redirectAttributes, Exception e){
+        redirectAttributes.addAttribute("message", e.getMessage());
+        return "redirect:/message";
     }
 }
