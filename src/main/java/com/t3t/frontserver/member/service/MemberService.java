b0cb28d2f2df82a6833d14e26db0f7cdc5b9e834
woody35545 <woody35545@gmail.com>
2024-05-05 21:16:09 +0900

    feat: #68 `MemberService` - 회원 비밀번호 변경 기능

--- src/main/java/com/t3t/frontserver/member/service/MemberService.java
+++ src/main/java/com/t3t/frontserver/member/service/MemberService.java
@@ -1,26 +1,24 @@
 package com.t3t.frontserver.member.service;
 
-import com.t3t.frontserver.member.client.MemberApiClient;
-import com.t3t.frontserver.member.exception.MemberApiClientException;
+import com.t3t.frontserver.member.adaptor.MemberAdaptor;
 import com.t3t.frontserver.member.model.dto.MemberAddressDto;
+import com.t3t.frontserver.member.model.request.MemberPasswordModifyRequest;
 import com.t3t.frontserver.member.model.request.MemberRegistrationRequest;
 import com.t3t.frontserver.member.model.response.MemberInfoResponse;
 import com.t3t.frontserver.member.model.response.MemberRegistrationResponse;
-import com.t3t.frontserver.model.response.BaseResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 
 @Slf4j
 @Service
 @RequiredArgsConstructor
 public class MemberService {
-    private final MemberApiClient memberApiClient;
+
+    private final MemberAdaptor memberAdaptor;
 
     /**
      * 회원 가입 요청
@@ -29,15 +27,7 @@ public class MemberService {
      * @author woody35545(구건모)
      */
     public MemberRegistrationResponse registerMember(MemberRegistrationRequest memberRegistrationRequest) {
-        ResponseEntity<BaseResponse<MemberRegistrationResponse>> responseEntity = memberApiClient.registerMember(memberRegistrationRequest);
-
-        if (!responseEntity.getStatusCode().is2xxSuccessful()) {
-            throw new MemberApiClientException("status code => " + responseEntity.getStatusCodeValue());
-        }
-
-        return Optional.ofNullable(responseEntity.getBody())
-                .map(BaseResponse::getData)
-                .orElseThrow(MemberApiClientException::new);
+        return memberAdaptor.registerMember(memberRegistrationRequest);
     }
 
 
@@ -48,32 +38,28 @@ public class MemberService {
      * @author woody35545(구건모)
      */
     public MemberInfoResponse getMemberInfoResponseById(long memberId) {
-        ResponseEntity<BaseResponse<MemberInfoResponse>> responseEntity = memberApiClient.getMemberById(memberId);
-
-        if (!responseEntity.getStatusCode().is2xxSuccessful()) {
-            throw new MemberApiClientException("status code => " + responseEntity.getStatusCodeValue());
-        }
-
-        return Optional.ofNullable(responseEntity.getBody())
-                .map(BaseResponse::getData)
-                .orElseThrow(MemberApiClientException::new);
+        return memberAdaptor.getMemberInfoResponseById(memberId);
     }
 
     /**
      * 회원 식별자로 특정 회원이 등록한 모든 회원 주소 정보들을 조회
+     *
      * @param memberId 조회하려는 회원의 식별자
      * @return 회원 주소 목록
      * @author woody35545(구건모)
      */
     public List<MemberAddressDto> getMemberAddressDtoListByMemberId(long memberId) {
-        ResponseEntity<BaseResponse<List<MemberAddressDto>>> responseEntity = memberApiClient.getMemberAddressListByMemberId(memberId);
+        return memberAdaptor.getMemberAddressDtoListByMemberId(memberId);
+    }
 
-        if (!responseEntity.getStatusCode().is2xxSuccessful()) {
-            throw new MemberApiClientException("status code => " + responseEntity.getStatusCodeValue());
-        }
-
-        return Optional.ofNullable(responseEntity.getBody())
-                .map(BaseResponse::getData)
-                .orElseThrow(MemberApiClientException::new);
+    /**
+     * 회원 비밀번호 변경
+     *
+     * @param memberId 회원 식별자
+     * @param request  비밀번호 변경 요청 정보
+     * @author woody35545(구건모)
+     */
+    public void modifyPassword(long memberId, MemberPasswordModifyRequest request) {
+        memberAdaptor.modifyPassword(memberId, request);
     }
 }
