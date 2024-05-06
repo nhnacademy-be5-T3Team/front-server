28a2ab7780492d475d65f4f594d8165a8192e263
woody35545 <woody35545@gmail.com>
2024-05-06 23:33:43 +0900

    modify: #69 `MyPageInfoViewDto` - 주소 관련 필드 수정

--- src/main/java/com/t3t/frontserver/member/model/dto/MyPageInfoViewDto.java
+++ src/main/java/com/t3t/frontserver/member/model/dto/MyPageInfoViewDto.java
@@ -27,5 +27,16 @@ public class MyPageInfoViewDto {
     private String gradeName;
     private String status;
     private String role;
-    private List<String> addressList;
+    private MemberAddressInfo defaultMemberAddressInfo;
+    private List<MemberAddressInfo> memberAddressList;
+
+    @Getter
+    @Builder
+    public static class MemberAddressInfo {
+        private Long id; // MemberAddress 식별자
+        private String roadNameAddress; // 도로명 주소
+        private String addressDetail; // 상세 주소
+        private String nickName; // 주소 별칭
+        private Boolean isDefault; // 기본 주소 여부
+    }
 }
