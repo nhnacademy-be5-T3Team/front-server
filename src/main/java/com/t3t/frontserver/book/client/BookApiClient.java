7f9e87a2490eb649a41411cb4ae5fea8e8496301
yujinKim <yujinkimvv@gmail.com>
2024-05-06 22:42:51 +0900

    feat : #144 도서 상세 수정 api client 구현

--- src/main/java/com/t3t/frontserver/book/client/BookApiClient.java
+++ src/main/java/com/t3t/frontserver/book/client/BookApiClient.java
@@ -1,5 +1,6 @@
 package com.t3t.frontserver.book.client;
 
+import com.t3t.frontserver.book.model.request.ModifyBookDetailRequest;
 import com.t3t.frontserver.book.model.response.BookDetailResponse;
 import com.t3t.frontserver.book.model.response.BookListResponse;
 import com.t3t.frontserver.model.response.BaseResponse;
@@ -7,6 +8,8 @@ import com.t3t.frontserver.model.response.PageResponse;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
 
 @FeignClient(name = "bookApiClient", url = "${t3t.feignClient.url}")
 public interface BookApiClient {
@@ -22,4 +25,15 @@ public interface BookApiClient {
      */
     @GetMapping(value = "/t3t/bookstore/books")
     ResponseEntity<BaseResponse<PageResponse<BookListResponse>>> getAllBooks(@RequestParam int pageNo, @RequestParam int pageSize);
+
+    /**
+     * 특정 도서의 상세 정보를 수정
+     * @param bookId 수정할 도서의 식별자
+     * @param request 수정할 도서의 상세 정보를 담은 요청 객체
+     * @return  200 OK, 성공 메세지
+     * @author Yujin-nKim(김유진)
+     */
+    @PutMapping(value = "/t3t/bookstore/books/{bookId}/book-detail")
+    ResponseEntity<BaseResponse<Void>> updateBookDetail(@PathVariable Long bookId,
+                                                        @RequestBody @Valid ModifyBookDetailRequest request);
 }
