6c1359b5fbdb7fa40f80b9d2815f0cbf114a8f7b
woody35545 <woody35545@gmail.com>
2024-05-06 23:18:34 +0900

    feat: #73 `MemberAddressController` - 회원 기본 주소 설정 및 변경 요청 처리

--- src/main/java/com/t3t/frontserver/member/controller/MemberAddressController.java
+++ src/main/java/com/t3t/frontserver/member/controller/MemberAddressController.java
@@ -7,6 +7,7 @@ import com.t3t.frontserver.member.service.MemberAddressService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -47,4 +48,21 @@ public class MemberAddressController {
 
         return "redirect:/mypage/address";
     }
+
+    /**
+     * 회원 기본 주소 설정 및 변경 요청 처리
+     */
+    @PostMapping("/member-addresses/default")
+    public String modifyDefaultAddress(@RequestParam("memberAddressId") Long memberAddressId) {
+
+        if (!SecurityContextUtils.isLoggedIn()) {
+            return "redirect:/login";
+        }
+
+        log.info("memberAddressId => {}", memberAddressId);
+        memberAddressService.modifyDefaultAddress(memberAddressId);
+
+        return "redirect:/mypage/address";
+    }
+
 }
