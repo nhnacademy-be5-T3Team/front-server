910b34cb2daf54104276c04b2d0276f24694b70c
woody35545 <woody35545@gmail.com>
2024-05-16 01:09:14 +0900

    feat: #104 `OrderService` - 페이징된 주문 관련 정보 조회 기능

--- src/main/java/com/t3t/frontserver/order/service/OrderService.java
+++ src/main/java/com/t3t/frontserver/order/service/OrderService.java
@@ -1,11 +1,15 @@
 package com.t3t.frontserver.order.service;
 
 
+import com.t3t.frontserver.model.response.PageResponse;
 import com.t3t.frontserver.order.adaptor.OrderAdaptor;
 import com.t3t.frontserver.order.model.request.MemberOrderPreparationRequest;
 import com.t3t.frontserver.order.model.request.OrderConfirmRequest;
 import com.t3t.frontserver.order.model.response.MemberOrderPreparationResponse;
+import com.t3t.frontserver.order.model.response.OrderInfoResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -30,5 +34,14 @@ public class OrderService {
      */
     public void confirmOrder(OrderConfirmRequest request) {
         orderAdaptor.confirmOrder(request);
+    }
+
+    /**
+     * 특정 회원의 모든 주문 관련 정보를 페이징을 통해 조회
+     *
+     * @author woody35545(구건모)
+     */
+    public PageResponse<OrderInfoResponse> getMemberOrderInfoListByMemberId(Long memberId, Pageable pageable) {
+        return orderAdaptor.getMemberOrderInfoListByMemberId(memberId, pageable);
     }
 }
