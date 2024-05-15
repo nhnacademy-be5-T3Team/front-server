5490d2948dd2b38620aa7369d0dd2750a6758d6b
woody35545 <woody35545@gmail.com>
2024-05-16 01:08:26 +0900

    feat: #104 `OrderApiClient` - 주문 정보 특정 회원의 모든 주문 관련 정보를 페이징을 통해 조회하는 API

--- src/main/java/com/t3t/frontserver/order/client/OrderApiClient.java
+++ src/main/java/com/t3t/frontserver/order/client/OrderApiClient.java
@@ -1,15 +1,17 @@
 package com.t3t.frontserver.order.client;
 
 import com.t3t.frontserver.model.response.BaseResponse;
+import com.t3t.frontserver.model.response.PageResponse;
 import com.t3t.frontserver.order.model.request.MemberOrderPreparationRequest;
 import com.t3t.frontserver.order.model.request.OrderConfirmRequest;
 import com.t3t.frontserver.order.model.response.MemberOrderPreparationResponse;
+import com.t3t.frontserver.order.model.response.OrderInfoResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "OrderApiClient", url = "${t3t.feignClient.url}")
 public interface OrderApiClient {
@@ -31,4 +33,12 @@ public interface OrderApiClient {
      */
     @PostMapping("/t3t/bookstore/orders/confirm")
     ResponseEntity<BaseResponse<Void>> confirmOrder(@RequestBody OrderConfirmRequest orderConfirmRequest);
+
+    /**
+     * 특정 회원의 모든 주문 관련 정보를 페이징을 통해 조회
+     *
+     * @author woody35545(구건모)
+     */
+    @GetMapping("/t3t/bookstore/members/{memberId}/orders")
+    ResponseEntity<BaseResponse<PageResponse<OrderInfoResponse>>> getMemberOrderInfoListByMemberId(@PathVariable("memberId") Long memberId, Pageable pageable);
 }
