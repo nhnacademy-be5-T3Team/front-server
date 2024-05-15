d64e069c7d9e8909a913dbe72a44b3a08d435cb7
woody35545 <woody35545@gmail.com>
2024-05-15 14:10:38 +0900

    feat: #101 `OrderApiClient` - 주문에 대한 결제 검증 및 주문 승인 API

--- src/main/java/com/t3t/frontserver/order/client/OrderApiClient.java
+++ src/main/java/com/t3t/frontserver/order/client/OrderApiClient.java
@@ -2,11 +2,14 @@ package com.t3t.frontserver.order.client;
 
 import com.t3t.frontserver.model.response.BaseResponse;
 import com.t3t.frontserver.order.model.request.MemberOrderPreparationRequest;
+import com.t3t.frontserver.order.model.request.OrderConfirmRequest;
 import com.t3t.frontserver.order.model.response.MemberOrderPreparationResponse;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @FeignClient(name = "OrderApiClient", url = "${t3t.feignClient.url}")
 public interface OrderApiClient {
@@ -20,4 +23,13 @@ public interface OrderApiClient {
     @PostMapping(value = "/t3t/bookstore/orders/member")
     ResponseEntity<BaseResponse<MemberOrderPreparationResponse>> createMemberOrder(@RequestBody MemberOrderPreparationRequest memberOrderPreparationRequest);
 
+
+    /**
+     * 주문에 대한 결제 검증 및 주문 승인 API
+     *
+     * @author woody35545(구건모)
+     */
+    @PostMapping("/orders/confirm")
+    @ResponseStatus(HttpStatus.OK)
+    ResponseEntity<BaseResponse<Void>> confirmOrder(OrderConfirmRequest orderConfirmRequest);
 }
