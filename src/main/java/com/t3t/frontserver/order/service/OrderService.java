ff62f7b1ac8a99c3041787d0a32a7735b843e53b
woody35545 <woody35545@gmail.com>
2024-05-15 15:15:54 +0900

    feat: #101 `OrderService` - 주문에 대한 결제 검증 및 주문 승인

--- src/main/java/com/t3t/frontserver/order/service/OrderService.java
+++ src/main/java/com/t3t/frontserver/order/service/OrderService.java
@@ -3,6 +3,7 @@ package com.t3t.frontserver.order.service;
 
 import com.t3t.frontserver.order.adaptor.OrderAdaptor;
 import com.t3t.frontserver.order.model.request.MemberOrderPreparationRequest;
+import com.t3t.frontserver.order.model.request.OrderConfirmRequest;
 import com.t3t.frontserver.order.model.response.MemberOrderPreparationResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -20,5 +21,14 @@ public class OrderService {
      */
     public MemberOrderPreparationResponse createMemberOrder(MemberOrderPreparationRequest memberOrderPreparationRequest) {
         return orderAdaptor.createMemberOrder(memberOrderPreparationRequest);
+    }
+
+    /**
+     * 주문에 대한 결제 검증 및 주문 승인
+     *
+     * @author woody35545(구건모)
+     */
+    public void confirmOrder(OrderConfirmRequest request) {
+        orderAdaptor.confirmOrder(request);
     }
 }
